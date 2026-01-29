5da1baa9fbe2ca49973238a701d7804f1a23eb24
codercoder61 <mohamedknoud@gmail.com>
2026-01-29 19:02:59 +0100

    Update API endpoint URLs for fetching anime data

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -69,7 +69,7 @@ export default function WatchPage() {
     try {
       setIsLoading(true);
       const response = await fetch(
-        `https://logic-association-short-circumstances.trycloudflare.com/getAnimeEpisodesInfo?episodeHref=https://anime3rb.com/episode/${animeId}/1&page=${currentPage}`
+        `https://eur-gst-demonstrate-circumstances.trycloudflare.com/getAnimeEpisodesInfo?episodeHref=https://anime3rb.com/episode/${animeId}/1&page=${currentPage}`
       );
       const data = await response.json();
       if (response.ok) {
@@ -94,7 +94,7 @@ const fetchEpisodeSource = async (number=1) => {
       try {
         setIsLoading(true)
         const response = await fetch(
-          `https://logic-association-short-circumstances.trycloudflare.com/getEpisodeSource?episodeHref=https://anime3rb.com/episode/${animeId}/${number}`
+          `https://eur-gst-demonstrate-circumstances.trycloudflare.com/getEpisodeSource?episodeHref=https://anime3rb.com/episode/${animeId}/${number}`
         )
         const data = await response.json()
         if (response.ok) {
@@ -121,7 +121,7 @@ const fetchEpisodeSource = async (number=1) => {
       try {
         setIsLoading(true)
         const response = await fetch(
-          `https://logic-association-short-circumstances.trycloudflare.com/getAnimeInfo?animeId=${animeId}`
+          `https://eur-gst-demonstrate-circumstances.trycloudflare.com/getAnimeInfo?animeId=${animeId}`
         )
         const data = await response.json()
                   setDesc(data.animeInfo.desc)
