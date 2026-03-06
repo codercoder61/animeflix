df7abca965a6b18ac603fcd3aee30e96712866b2
codercoder61 <mohamedknoud@gmail.com>
2026-03-07 00:33:02 +0100

    Update search-bar.tsx

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -13,7 +13,7 @@ export default function SearchBar() {
 const searchAnime = async (query: string, requestId: number) => {
   try {
     const response = await fetch(
-      `http://192.168.0.176:3001/search?q=${query}`, {
+      `https://pennsylvania-tournament-railroad-real.trycloudflare.com/search?q=${query}`, {
   headers: {
     "ngrok-skip-browser-warning": "true"
   }
@@ -111,7 +111,7 @@ useEffect(() => {
                <img
   src={
     anime.poster
-      ? `http://192.168.0.176:3001/image-proxy?url=${anime.poster}`
+      ? `https://pennsylvania-tournament-railroad-real.trycloudflare.com/image-proxy?url=${anime.poster}`
       : "/placeholder.svg"
   }
   alt={anime.title}
