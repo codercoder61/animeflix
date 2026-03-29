b68d407756fded8a58169faaa5da5c61feaa92dd
codercoder61 <mohamedknoud@gmail.com>
2026-03-29 20:17:46 +0200

    Update search-bar.tsx

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -13,7 +13,7 @@ export default function SearchBar() {
 const searchAnime = async (query: string, requestId: number) => {
   try {
     const response = await fetch(
-      `https://classic-duration-lawrence-chapter.trycloudflare.com/search?q=${query}`, {
+      `https://evanescence-however-scholars-zum.trycloudflare.com/search?q=${query}`, {
   headers: {
     "ngrok-skip-browser-warning": "true"
   }
@@ -111,7 +111,7 @@ useEffect(() => {
                <img
   src={
     anime.poster
-      ? `https://classic-duration-lawrence-chapter.trycloudflare.com/image-proxy?url=${anime.poster}`
+      ? `https://evanescence-however-scholars-zum.trycloudflare.com/image-proxy?url=${anime.poster}`
       : "/placeholder.svg"
   }
   alt={anime.title}
